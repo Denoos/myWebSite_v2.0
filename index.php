b775188c65287f25a90b33f0cbfeaff3c7bfd808
Данил Тимофеев <danil2006timofeev@gmail.com>
2024-10-11 22:38:26 +1000

    start.OnlyStructureReady();

--- index.php
+++ index.php
@@ -4,3 +4,12 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
 include_once './inc/function.php';
+
+include 'template/frontend/partials/head.php';
+
+include 'template/frontend/partials/header.php';
+include 'template/frontend/partials/searchForm.php';
+include 'template/frontend/partials/main.php';
+
+include 'template/frontend/partials/skripts.php';
+
